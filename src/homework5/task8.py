7b98eeb3a0a111d366bfe21cf04a23de3006bfe8
Pmoshko <pmoshko@gmail.com>
2024-02-28 16:37:25 +0300

    ДЗ 5

---
+++ src/homework5/task8.py
@@ -0,0 +1,4 @@
+b = [1, 2, 3, 4]
+c = ['a', 'b', 'c', 'd']
+g = {k:v for k in b for v in c }
+print(g)
--- todo.py
+++ todo.py
@@ -1,14 +1,33 @@
-# student = int(input())
-# l = []
-# for v in range(student):
-#     v = int(input())
-#     for z in range(v):
-#         z = input()
-#         l.append(z)
+class Person():
+    def __init__(self, name, surname, age, place_of_birth):
+        self.name = name
+        self.surname = surname
+        self.age = age
+        self.place_of_birth = place_of_birth
+    def get_age(self, year):
+        print(year - self.age)
 
+p1 = Person("Pavel", "Moshko", 1990, "Republic of Belarus")
+p2 = Person("Oksana", "Marchenko", 1990, "Republic of Belarus")
+p3 = Person("Nikita", "Moshko", 2018, "Republic of Belarus")
+p4 = Person("Zlata", "Moshko", 1990, "Republic of Belarus")
 
-f = ['Russian', 'English', 'Russian', 'Belarusian', 'English', 'Russian', 'Italian', 'French']
-for count, value  in enumerate(f):
-    if f.count(value) == 3:
-        print(len(count))
+p2.get_age(2024)
+print(p1.name, p2.name, p3.name, p4.name)
 
+class Circle():
+    def __init__(self, radius):
+        self.radius = radius
+
+    def get_pirimetre(self):
+        return self.radius * 2 * 3.14
+    def get_area(self):
+        return 3.14 * self.radius ** 2
+
+c1 = Circle(3)
+print(c1.get_pirimetre())
+print(c1.get_area())
+
+c2 = Circle(7)
+print(c2.get_pirimetre())
+print(c2.get_area())
