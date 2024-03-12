cff8ab813ef43e7217e7aefca0f4da2b7b38777a
Pmoshko <pmoshko@gmail.com>
2024-03-12 17:26:40 +0300

    ДЗ 6

--- test 2.py
+++ test 2.py
@@ -1,54 +1,38 @@
-# def isPalindrome(n1 = int):
-#     digit = n1 % 10
-#     n2 = digit
-#     n1 = n1 // 10
-#     while n1 > 0:
-#         digit = n1 % 10
-#         n1 = n1 // 10
-#         n2 = n2 * 10
-#         n2 = n2 + digit
-#
-#
-#
-# isPalindrome(1243234)
-#
-# def plusOne(a = []):
-#     b = ("".join(map(str,a)))
-#     c = int(b) + 1
-#     print(c)
-# plusOne()
+class Building:
+    def __init__(self, wall_1: float, wall_2: float, number_of_storeys: int):
+        self.wall_1 = wall_1
+        self.wall_2 = wall_2
+        self.number_of_storeys = number_of_storeys
+
+    def floor_area(self):
+        return print(f" Площадь здания:{self.wall_1*self.wall_2}")
+
+class Cars:
+    def __init__(self, length: float, width: float, power: int, volume: float, class_car: str, prise: float):
+        self.length = length
+        self.width = width
+        self.power = power
+        self.volume = volume
+        self.class_car = class_car
+        self.prise = prise
+
+    def parking_space_size(self):
+        if isinstance(self, Cars):
+            return print(f'парковочная площадь: {self.length*self.width}')
+class ShowRoom(Building):
+    cars_showroom = []
+    def __init__(self, wall_1: float, wall_2: float, number_of_storeys: int, number_of_car_space: int):
+        super().__init__(wall_1, wall_2, number_of_storeys )
+        self.number_of_car_space = number_of_car_space
+
+    def add_car(self):
+        self.cars_showroom.append()
+        return print(self.cars_showroom)
 
 
-student = int(input())
-l = []
-for v in range(student):
-  v = int(input())
-    for z in range(1, v +1):
-        z = input()
-        l.append(z)
-for w in range(len(l)):
-    if l.count(l[w]) == student:
-        print(l[w])
-    if l.count(l[w]) == 1:
-        print(l[w])
+if __name__ == "__main__":
+    bmw = Cars(3,2, 300, 2.5, "b",100000)
+    audi = Cars(5.3,2, 400, 2.5, "b",300000)
 
-
-    # h = {}
-    # for g in range(len(l)):
-    #     if l[g] in h:
-    #         h[l[g]] += 1
-    #     else:
-    #         h[l[g]] = 1
-    # for k, v in h.values():
-    #     if h.values() : 1:
-    #         print(k)
-
-# r = []
-# for w in range(len(l)):
-#     if l.count(l[w]) == student:
-#         print(l.count(l[w]) // student)
-#         r.append(l[w])
-#         print(l[w])
-#         print(len(set(r)))
-#     if l.count(l[w]) == 1:
-#         print(l[w])
+    b1 = ShowRoom(5,3,2,5)
+    ShowRoom.add_car(bmw)
--- test.py
+++ test.py
@@ -1,70 +1,23 @@
-# nums = [2, 7, 11, 15]
-# t = 9
-# for i in range(len(nums)):
-#     for j in range(i+1, len(nums)):
-#         if nums[i] + nums[j] == t:
-#             print([i],[j])
+from datetime import datetime, date
 
-# nums = [2, 7, 11, 15]
-# t = 9
-# for i in range(len(nums)):
-#     if nums[i] + nums[i+1] == t:
-#         print([i],[i+1])
-#
-# f = 123321
-# d = []
-# for i in range(f):
-#     d.append(i)
-#     print(len(d))
+class Staff():
 
+    number_of_staff = 0
+    def __init__(self, surname: str, name: str, age: date, position: str):
+        self.surname = surname
+        self.name = name
+        self.age = date
+        self.position = position
+        Staff.number_of_staff = Staff.number_of_staff + 1
 
+    @classmethod
+    def total_object(cls):
+        print(f"Total number of stuff:{ cls.number_of_staff}")
 
+    @classmethod
+    def name(cls):
+        print(cls.__name__)
 
-# def countries(*args, **kwargs):
-#     c = int(input())
-#     g = {}
-#     for i in range(1, c + 1):
-#         i = input()
-#         i = i.split()
-#         for j in range(1, len(i)):
-#             g[i[j]] = i[0]
-#     d = int(input())
-#     p = []
-#     for m in range(1, d + 1):
-#         m = input()
-#         p.append(m)
-#     for x in p:
-#         print(g.get(x))
-# countries(2,3)
-
-# a = {i:i**3 for i in range(1,21)}
-# print(a)
-
-# a = [1, 2, 1, 5, 6, 5, 4, 3, 2, 1]
-# b = [6, 5, 9, 5, 6, 5, 4, 5, 8, 1]
-# c = a + b
-# x = {}
-# for i in c:
-#     if i in x:
-#         x[i] +=1
-#     else:
-#         x[i] = 1
-# print(len(x))
-
-# a = [1, 2, 1, 5, 6, 5, 4, 3, 2, 1]
-# b = [8, 5, 5, 5, 8, 5, 4, 5, 8, 1]
-# c = {}
-# x = {}
-# for i in a:
-#     if i in c:
-#         c[i] += 1
-#     else:
-#         c[i] = 1
-# for j in b:
-#     if j in x:
-#         x[j] += 1
-#     else:
-#         x[j] = 1
-# print(len(x), len(c))
-
-
+p1 = Staff('Mashko', 'Pavel', 1990, 'boss')
+Staff.total_object()
+Staff.name()
